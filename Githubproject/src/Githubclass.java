1fc0e178913a1ede6c8d0a7619699b16dfc2fcfc
Seerthy J <SeerthyJayaramanJ@sulekha.com>
2018-04-10 14:58:55 +0530

    Modified by seerthy

--- Githubproject/src/Githubclass.java
+++ Githubproject/src/Githubclass.java
@@ -8,7 +8,8 @@ public class Githubclass {
 		System.out.println("Modified");
 		
 		
-		System.out.println("Modified BY SEERTHY");
+		System.out.println("Modified BY SEERTHYJAYARAMAN");
+		
 	}
 
 }
